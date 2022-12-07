a83f66184e56f4ee9e0210a9e132f3edcbcf5660
Carlos Restrepo <crestr25@gmail.com>
2022-12-07 16:13:18 -0500

    started section 3

---
+++ Go/Udemy/Go Developers Guide/Section_3/main.go
@@ -0,0 +1,64 @@
+package main
+
+import "fmt"
+
+// Defining a new contactInfo struct
+type contactInfo struct {
+    email string
+    zipCode int
+}
+// Defining a importantPerson struct
+type importantPerson struct {
+    firstName string
+    lastName string
+    contact contactInfo // We can ommit the naming and it just defaults to the name of the
+    // struct
+}
+// Defining a new struct person
+type person struct {
+    firstName string
+    lastName string
+}
+
+func main() {
+    // Basic structs
+    basicStructs()
+    // Embedded Structs
+    embeddedStructs()
+}
+
+func embeddedStructs() {
+    // Declaring a person struct with embedded stuff 
+    carlos := importantPerson{
+        firstName: "Carlos",
+        lastName: "Restrepo",
+        contact: contactInfo{
+            email: "crestrepo@mail.com",
+            zipCode: 1,
+        }, // must have a comma man!!!
+    }
+    fmt.Println(carlos)
+    fmt.Printf("%+v", carlos) // print every value, we need to pass a string and format it 
+}
+
+func basicStructs() {
+    // first way of defining a new structure
+    luisa := person{"Luisa", "Fonseca"}
+    // Secong way of declaring a structure
+    carlos := person{firstName: "Carlos", lastName: "Restrepo"}
+    // Third way of creating a variable
+    var pia person // This way a Zero value gets assigned
+    // "" - Strings
+    // 0  - Int
+    // 0  - Float
+    // bool - false
+
+    fmt.Println(carlos) 
+    fmt.Println(luisa)
+    fmt.Printf("%+v", pia) // print every value, we need to pass a string and format it 
+    
+    // add values to the fields of the structs
+    pia.firstName = "Pia"
+    pia.lastName = "Fonseca"
+    fmt.Printf("\n%+v\n", pia) // print every value, we need to pass a string and format it 
+}
