ab8725ba02bfcd0a2ca1cb55bbcb8dc872dd46d4
Carlos Restrepo <crestr25@gmail.com>
2023-08-01 16:43:53 -0500

    .

--- Theory/Design Patterns/Behavioral/Iterator/Implementation/iterator.py
+++ Theory/Design Patterns/Behavioral/Iterator/Implementation/iterator.py
@@ -1,27 +1,78 @@
-class Node:
-    def __init__(self, value, left=None, right=None) -> None:
-        self.right = right
-        self.left = left
-        self.value = value
+from collections.abc import Iterable, Iterator
+from typing import Any, List
 
-        self.parent = None
+"""
+There are two abstract classes in python
 
-        if left:
-            self.left.parent = self
-        if right:
-            self.right.parent = self
+1. the iterable class, which implements the __iter__() method (The collection)
+2. the iterator class, which implements the __next__() method (The iterator)
+"""
 
-class InOrderIterator:
-    def __init__(self, root):
-        self.root = self.current = root
-        self.yielded_start = False
-        while self.current.left:
-            self.current = self.current.left
+class AlphabeticalOrderIterator(Iterator):
+    """
+    This is the concrete class for the iterator which will
+        - Implement the algorithm to traverse the collection
+        - Store the current traversal position at all times
+    """
+
+    # First we need a _position attribute to store the actual position.
+    # This can be extended to have multiple fields if the collection needs it
+    _position: int = None
+
+    # Indicate the traversal direction
+    _reverse: bool = False
+
+    def __init__(self, collection, reverse: bool = False) -> None:
+        self._collection = collection
+        self._reverse = reverse
+        self._position = -1 if reverse else 0
+
+    def __next__(self):
+        """
+        To implement the iterator the next method should return
+            - The next item in the sequence.
+            - Raise StopIteration exception
+        """
+        try:
+            value = self._collection[self._position]
+            self._position += 1 if self._reverse else 1
+        except IndexError:
+            raise StopIteration()
+
+        return value
+
+class WordsCollection(Iterable):
+    """
+    This is the Concrete collection implementation, it should
+        - return the iterator object that is compatible with the collection
+    """
+    def __init__(self, collection: List[Any] = []) -> None:
+        self._collection = collection
+
+    def __iter__(self) -> AlphabeticalOrderIterator:
+        """
+        The iter method returns the iterator object.
+        """
+        return AlphabeticalOrderIterator(self._collection)
+
+    def add_item(self, item: Any):
+        self._collection.append(item)
 
 if __name__ == "__main__":
-    #    1
-    #   / \
-    #  2   3
+    """
+    The client code will work with the collection
+    """
 
-    root = Node(1, Node(2), Node(3))
-    print(root)
+    collection = WordsCollection()
+    collection.add_item("A")
+    collection.add_item("B")
+    collection.add_item("C")
+
+    print("".join(i for i in collection))
+
+    # it = iter(collection)
+
+    # for i in range(10):
+    #     print(next(it))
+
+
